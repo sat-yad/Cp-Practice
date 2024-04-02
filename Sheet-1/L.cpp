8e9e8af8c1f67e178feb30cb22762ce0e6baa6ae
satyam <skill.improve24@gmail.com>
2024-04-02 16:17:11 +0530

    updated sheet-01

---
+++ Sheet-1/L.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fl(w,x) for(ll i=w;i<x;i++)
+#define fast   ios_base::sync_with_stdio(0);cin.tie(0);cin.tie(nullptr); cout.tie(nullptr);
+#define nl cout<<"\n";
+#define onjudge  #ifndef ONLINE_JUDGE freopen("./input.txt", "r", stdin); freopen("./output.txt", "w", stdout);#endif
+void solve() {
+  string s1,s2,s3,s4;
+  cin>>s1>>s2;
+  cin>>s3>>s4;
+  if(s2==s4) cout<<"ARE Brothers"<<endl;
+  else cout<<"NOT"<<endl;
+}
+int main(){
+    fast;
+ solve();
+}
